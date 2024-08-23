c1287048ef077337cf3af09c4b0cd84687722fc2
Nii-101 <noelamptey101@gmail.com>
2024-08-23 19:51:19 +0000

    style: finished styling the main pages and the payment section

--- frontend-foodie/src/component/selectedMenu.jsx
+++ frontend-foodie/src/component/selectedMenu.jsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import {useAppContext} from '../component/context';
 import {menu, order, url} from "../server";
 import {useEffect, useState} from "react";
@@ -14,6 +14,7 @@ export const SelectedMenu = () => {
     const [deliveryAddress, setDeliveryAddress] = useState('');
     const [paymentMethod, setPaymentMethod] = useState('');
     const [pack, setPack] = useState('');
+    const go = useNavigate();
 
     
     useEffect(() => {
@@ -64,78 +65,96 @@ export const SelectedMenu = () => {
                 withCredentials: true
             });
             alert(`You have successfully placed an order with the id: ${res.data.msg._id}`);
+            go('/cart')
         } catch (error) {
             alert(error.response.data.msg);
         }
     }
 
     return (
-        <div className="res-order">
-            <section className="res-order-center">
-                <img src={`${url}/upload/${selectedMenu.file}`} alt="" height='200px' width='200px' />
-            </section>
-            <div className='res-order-center-det'>
-                <h2>{selectedMenu.name}</h2>
-                <p>description: {selectedMenu.description}</p>
-                <div className='ind'>
-                    <span>Price: </span>
-                    <span>&#8358;{selectedMenu.price}</span>
-                </div>
-                <div className='ind'>
-                    <span>Total amount: </span>
-                    <span>&#8358;{selectedMenu.price * quantity}</span>
-                </div>
-            </div>
-            <form action="" onSubmit={placeAnOrder}>
-                <div>
-                    <p>address</p>
-                    <input
-                        type="text"
-                        value={deliveryAddress}
-                        onChange={(e) => setDeliveryAddress(e.target.value)}
-                        placeholder="delvey address"
-                    />
-                </div>
-                <div>
-                    <p>Payment Method</p>
-                    <select
-                        name=""
-                        id=""
-                        value={paymentMethod}
-                        onChange={(e) => setPaymentMethod(e.target.value)}
-                    >
-                        <option value="">Payment</option>
-                        <option value="Card">Card</option>
-                        <option value="Cash">Cash</option>
-                        <option value="Online">Online</option>
-                    </select>
-                </div>
+        <div className='w-full flex justify-center'>
 
-                <div>
-                    <p>Pack</p>
-                    <select
-                        name=""
-                        id=""
-                        value={pack}
-                        onChange={(e) => setPack(e.target.value)}
-                    >
-                        <option value="">pack</option>
-                        <option value="Big Pack">Big Pack</option>
-                        <option value="Branded Pack">Branded Pack</option>
-                    </select>
+            <div className="res-order bg-gray-200 pb-4 rounded-md min-w-[290px] max-w-[500px] ">
+                <section className="w-[100%] flex justify-center rounded-md rounded-b-none">
+                    <img className='rounded-[inherit] w-[full]' src={`${url}/upload/${selectedMenu.file}`} alt={`picture of ${selectedMenu.name}`} />
+                </section>
+                    <div  className='bg-orange-500 text-white p-1 rounded-t-md mt-4'>
+                        <h2 className='text-center capitalize font-semibold '>{selectedMenu.name}</h2>
+                        <p  className='text-sm font-medium text-center '><em>{selectedMenu.description}</em></p>
+                    </div>
+                <div className=' mt-4 p-2'>
+                    <div className='ind bg-gray-200'>
+                        <span className='font-semibold'>Price: </span>
+                        <span>&#8358;{selectedMenu.price}</span>
+                    </div>
+                    <div className='ind'>
+                        <span className='font-semibold'>Total amount: </span>
+                        <span>&#8358;{selectedMenu.price * quantity}</span>
+                    </div>
                 </div>
-                <div>
-                    <p>Quantity</p>
-                    <input
-                        type="number"
-                        placeholder="quantity"
-                        value={quantity}
-                        onChange={(e) => setQuantity(e.target.value)}
-                    />
-                </div>
-                <button>Place order</button>
-            </form>
-            {/* <Outlet /> */}
+                <form 
+                    className='px-2'
+                    action="" onSubmit={placeAnOrder}>
+                    <div>
+                        <label className='font-semibold' htmlFor='address'>Address</label>
+                        <input
+                            className='font-medium rounded-md'
+                            id='address'
+                            type="text"
+                            value={deliveryAddress}
+                            onChange={(e) => setDeliveryAddress(e.target.value)}
+                            placeholder="delivery address"
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label className='font-semibold' htmlFor='payMethod'>Payment Method</label>
+                        <select
+                            className='font-medium rounded-md'
+                            name=""
+                            id="payMethod"
+                            value={paymentMethod}
+                            onChange={(e) => setPaymentMethod(e.target.value)}
+                            aria-label='choose method of payment'
+                            required
+                        >
+                            <option value="Card">Card</option>
+                            <option value="Cash">Cash</option>
+                            <option value="Online">Online</option>
+                        </select>
+                    </div>
+
+                    <div>
+                        <label className='font-semibold' htmlFor='packSize'>Pack Size</label>
+                        <select
+                            className='font-medium rounded-md'
+                            name=""
+                            id="packSize"
+                            value={pack}
+                            onChange={(e) => setPack(e.target.value)}
+                            aria-label='choose pack size'
+                            required
+                        >
+                            <option value="Big Pack">Big Pack</option>
+                            <option value="Branded Pack">Branded Pack</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label className='font-semibold' htmlFor='quantity'>Quantity</label>
+                        <input
+                            aria-label='select quantity'
+                            className='rounded-md font-medium'
+                            type="number"
+                            placeholder="quantity"
+                            value={quantity}
+                            onChange={(e) => setQuantity(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <button className='rounded-md'>Place order</button>
+                </form>
+                {/* <Outlet /> */}
+            </div>
         </div>
      )
 }
--- frontend-foodie/src/pages/Menu.jsx
+++ frontend-foodie/src/pages/Menu.jsx
@@ -1,67 +1,73 @@
 import axios from "axios";
-import {menu, url} from '../server';
-import {useAppContext} from '../component/context';
-import {useEffect, useState} from 'react';
-import {useNavigate} from 'react-router-dom';
-
+import { menu, url } from "../server";
+import { useAppContext } from "../component/context";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import FoodieAnimation from "../component/foodieAnimation";
 
 export const Menu = () => {
-    const {state, dispatch} = useAppContext();
-    const [loading, setLoading] = useState(true);
-    const go = useNavigate();
+  const { state, dispatch } = useAppContext();
+  const [loading, setLoading] = useState(true);
+  const go = useNavigate();
 
-
-    useEffect(() => {
-        async function getMenu() {
-            try {
-                const { data } = await axios.get(`${menu}/all`);
-                //console.log(data)
-                dispatch({ type: 'SET_MENU', payload: data });
-            } catch (error) {
-                console.log(error);
-            } finally {
-                setLoading(false);
-            }
-        }
-        getMenu()
-    }, [dispatch]);
-
-    if(!state.menu){
-        return <div>
-            loading...
-        </div>
+  useEffect(() => {
+    async function getMenu() {
+      try {
+        const { data } = await axios.get(`${menu}/all`);
+        //console.log(data)
+        dispatch({ type: "SET_MENU", payload: data });
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
     }
+    getMenu();
+  }, [dispatch]);
 
+  if (!state.menu) {
+    return <div>loading...</div>;
+  }
 
-    if (loading) {
-        return <div>
-            loading....
-        </div>
-    }
-
-
+  if (loading) {
     return (
-        <div className="menu-item-block">
-            <h1>Available Menu</h1>
-            <section className="menu">
-                {
-                    state.menu && state.menu.map(data => {
-                        return (
-                            <div
-                                className="menu-center"
-                                key={data._id}
-                                onClick={() => {
-                                    go(`/selected-menu/${data._id}`)
-                                }}
-                            >
-                                <img src={`${url}/upload/${data.file}`} alt="" />
-                                <p>{data.name}</p>
-                                <button>Checkout</button>
-                            </div>
-                        )
-                    })
-                }
-            </section>
-        </div>
-    )
-}
+      <div>
+        <FoodieAnimation />
+      </div>
+    );
+  }
+
+  return (
+    <div className="menu-item-block">
+      <h1 className=" text-xl font-bold mb-4">Available Menu</h1>
+      <section className="px-2 max-w-6xl mx-auto grid gap-6 lg:gap-12 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center">
+        {state.menu &&
+          state.menu.map((data) => {
+            return (
+              <div
+                className="lg:hover:scale-[1.05] hover:shadow-xl shadow-gray-700 duration-300 cursor-pointer w-full max-w-[300px] border border-gray-300 bg-orange-500 rounded-md overflow-hidden"
+                key={data._id}
+                onClick={() => {
+                  go(`/selected-menu/${data._id}`);
+                }}
+              >
+                <img
+                  className="w-full h-32 md:h-48 object-cover"
+                  src={`${url}/upload/${data.file}`}
+                  alt=""
+                />
+                <div className="p-2 lg:pt-2 lg:pb-4 px-4">
+                  <p className="text-center text-sm sm:text-base lg:text-lg text-white font-semibold capitalize tracking-wider">
+                    {data.name}
+                  </p>
+                  <button className="font-[Roboto]  w-full bg-white text-orange-500 text-base font-bold rounded-md lg:hover:bg-white/90 lg:hover:text-green-800 transition duration-300">
+                    Checkout
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+      </section>
+    </div>
+  );
+};
